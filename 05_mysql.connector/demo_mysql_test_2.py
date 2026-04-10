aca02151d36792ff1812159fe5fa1fcb5a097d54
Fabio Guilherme <fabio.guilherme@gmail.com>
2026-04-10 12:05:35 +0100

    Update demo_mysql_test_2.py

--- 05_mysql.connector/demo_mysql_test_2.py
+++ 05_mysql.connector/demo_mysql_test_2.py
@@ -97,9 +97,22 @@ for x in myresult:
 '''
 
 # Wildcard Characters
+'''
 sql = "SELECT * FROM customers WHERE address LIKE '%way%'"
 
 mycursor.execute(sql)
+
+myresult = mycursor.fetchall()
+
+for x in myresult:
+  print(x)
+'''
+
+# Prevent SQL Injection
+sql = "SELECT * FROM customers WHERE address = %s"
+adr = ("Yellow Garden 2", )
+
+mycursor.execute(sql, adr)
 
 myresult = mycursor.fetchall()
 
